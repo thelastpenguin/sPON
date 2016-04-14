dc359f603eb6573ecb460370738e1d4876fc2fa8
Gareth George <garethgeorge97@gmail.com>
2016-04-14 03:12:04 -0700

    support for basic datatype decoding + references

---
+++ spon.lua
@@ -0,0 +1,163 @@
+-- localizations
+local select = select 
+local format_string = string.format 
+local concat = table.concat 
+local len = string.len 
+local string_find = string.find 
+local string_sub = string.sub 
+local tonumber = tonumber 
+local tostring = tostring 
+local math_log = math.log
+local math_ceil = math.ceil 
+-- caches
+
+	local max_stack = 128
+	local max_format_strings = max_stack + 32
+
+	-- string format string
+	local concat_format_strings = {'%s'}
+	for i = 2, max_format_strings do concat_format_strings[i] = concat_format_strings[i - 1] .. '%s' end
+
+	-- hex number cache
+	local hex_cache = {} for i = 0, 15 do hex_cache[format_string('%x', i)] = i end 
+
+
+local spon2 = {}
+
+
+local encoder = {}
+
+local log16 = math_log(16)
+
+encoder['number'] = function(value)
+	if value % 1 == 0 then
+		if value == 0 then return 'I0' end
+		if value < 0 then
+			return format_string('i%x%x', math_ceil(math_log(-value+1) / (log16)), -value)
+		else
+			return format_string('I%x%x', math_ceil(math_log(value+1) / (log16)), value)
+		end
+	else
+		if value < 0 then
+			return 'f' .. tostring(-value) .. ';'
+		else
+			return 'F' .. tostring(value) .. ';'
+		end
+	end
+end
+
+encoder['string'] = function(value, cache)
+	local len = len(value)
+	if len >= 16 * 16 then
+		return format_string('T%06X%s', len, value)
+	else
+		return format_string('S%02X%s', len, value)
+	end
+end
+
+
+local function fast_concat_stack(...)
+	local size = select('#', ...)
+	local last = select(size, ...)
+	if size == 1 then
+		return ...
+	elseif type(last) == 'function' then -- must never be greater than concat_format_strings count!!!
+		return format_string(concat_format_strings[size-1], ...), fast_concat_stack(last())
+	elseif size > max_format_strings then
+		return concat {...}
+	else
+		return format_string(concat_format_strings[size], ...)
+	end
+end
+
+local function encode_pairs(size, iterator, table, key, cache)
+	if size >= max_stack then
+		return function() return encode_sequential(0, iterator, table, k, cache) end
+	end
+	local k, v = iterator(table, key)
+	if k == nil then return '}' end
+	return encoder[type(k)](k, cache), encoder[type(v)](v, cache), encode_pairs(size + 2, iterator, table, k, cache)
+end
+
+
+local function encode_sequential(size, table, key, cache)
+	if size >= max_stack then 
+		return function() return encode_sequential(0, table, key, cache) end
+	end
+
+	key = key + 1
+	local value = table[key]
+	if value == nil then 
+		return '~', encode_pairs(size + 1, pairs(table), table, key ~= 1 and key - 1 or nil, cache)
+	end
+
+	return encoder[type(value)](value, cache), encode_sequential(size + 1, table, key, cache)
+end
+
+encoder['table'] = function(value, cache)
+	if cache[value] then
+		return format_string('(%x)', value)
+	end
+	return fast_concat_stack(fast_concat_stack('{',encode_sequential(1, value, 0, cache)))
+end
+
+local decoder = {}
+-- a short string with a 2-digit length component
+decoder['S'] = function(str, index, cache)
+	local strlen = tonumber(string_sub(str, index + 1, index + 2), 16)
+	return string_sub(index + 3, index + 3 + strlen), index + (3 + 1) + strlen
+end
+-- a long string with a 6-digit length component
+decoder['T'] = function(str, index, cache)
+	local strlen = tonumber(string_sub(str, index + 1, index + 6), 16)
+	return string_sub(index + 3, index + 3), index + (7 + 1) + strlen -- figure out if alignment is off i think its right
+end
+decoder['I'] = function(str, index, cache)
+	local digitCount = hex_cache[string_sub(str, index+1, index+1)]
+	if digitCount == 0 then return 0, index + 1 end
+	return tonumber(string_sub(str, index + 2, index + 1 + digitCount), 16), (2 + digitCount + 2)
+end
+
+decoder['{'] = function(str, index, cache)
+	index = index + 1
+	local table = {}
+
+	-- decode the array portion of the table
+	local i = 1
+	while true do
+		local c = string_sub(str, index, index)
+		if c == '~' or c == '}' or c == nil then break end
+		print(str, index, c)
+		table[i], index = decoder[c](str, index, cache)
+		i = i + 1
+	end
+
+	-- decode the key-value poriton of the table
+	local k
+	while true do
+		local c = string_sub(str, index, index)
+		if c == '}' or c == nil then break end
+		k, index = decoder[c](str, index, cache)
+		c = string_sub(str, index, index)
+		table[k], index = decoder[c](str, index, cache)
+	end
+
+	return table, index
+end
+
+
+
+
+spon2.encode = function(table)
+	return encoder.table(table, {})
+end
+
+spon2.decode = function(str)
+	return decoder['{'](str, 0, {})
+end
+
+local encoded = spon2.encode {1,2,3}
+print (encoded)
+print (spon2.encode(spon2.decode(encoded)))
+
+return spon2
